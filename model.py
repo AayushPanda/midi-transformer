e7371d0841d16abfcd52116473b9081bc366b7f7
Aayush Panda <aayush.vinayak@gmail.com>
2026-01-24 21:15:32 -0500

    greedy decoding inference

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
+from typing import Literal
 import torch
 import torch.nn as nn
 
 from attention import MultiHeadAttention
 from layer_norm import LayerNorm
 from feed_forward_mlp import MLP
+from tokeniser import BPETokeniser
 
 class Block(nn.Module):
     def __init__(self, in_dims, attention_dims, n_attn_heads, *args, **kwargs) -> None:
@@ -23,9 +25,11 @@ class Block(nn.Module):
         return x
 
 class Transformer(nn.Module):
-    def __init__(self, embedding_dims, n_blocks, attention_dims, n_attn_heads, context_length, vocab_length, *args, **kwargs):
+    def __init__(self, embedding_dims, n_blocks, attention_dims, n_attn_heads, context_length, vocab_length, tokeniser, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.tokeniser: BPETokeniser = tokeniser
+        self.context_length = self.tokeniser
         self.embeddings = nn.Parameter(torch.randn(vocab_length, embedding_dims))
         
         # figure this one out
@@ -38,9 +42,32 @@ class Transformer(nn.Module):
 
         self.reproj = nn.Linear(embedding_dims, vocab_length)
 
-    def forward(self, x):
+    def forward(self, x) -> torch.Tensor:
         tok_e = self.embeddings[x]
         pos_e = self.pos_embeddings[x]
         e = tok_e+pos_e
 
         return self.reproj(self.blocks(e))
+
+    def inference(self, prompt: str, max_tokens: int = 500, decoding_mode: Literal["greedy"] | Literal["beam"] | Literal["nucleus"] = "greedy"):
+        tokens = self.tokeniser.encode(prompt)
+        if len(tokens) > max_tokens: raise ValueError(f"Context window exceeded: max {self.max_tokens}, got {len(tokens)}")
+        output = []
+        for _ in range(max_tokens):
+            token_probs = torch.softmax(self.forward(tokens), 0)
+            if decoding_mode=="greedy":
+                output.append(self.tokeniser.decode(torch.argmax(token_probs)))
+
+        return "".join(output)
+    
+    def inference_stream(self, prompt: str, max_tokens: int = 500, decoding_mode: Literal["greedy"] | Literal["beam"] | Literal["nucleus"] = "greedy"):
+        tokens = self.tokeniser.encode(prompt)
+        if len(tokens) > max_tokens: raise ValueError(f"Context window exceeded: max {self.max_tokens}, got {len(tokens)}")
+
+        for _ in range(max_tokens):
+            token_probs = torch.softmax(self.forward(tokens), 0)
+            if decoding_mode=="greedy":
+                yield self.tokeniser.decode(torch.argmax(token_probs))
+
+
+            
