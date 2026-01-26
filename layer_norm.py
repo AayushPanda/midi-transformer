5c846016c54345da93f0f78fe951bc6a2084b81f
Aayush Panda <aayush.vinayak@gmail.com>
2026-01-26 11:36:23 -0500

    fix layernorm initial state

--- layer_norm.py
+++ layer_norm.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 
 class LayerNorm(nn.Module):
 
-    def __init__(self, shape, eps=10e-10):
+    def __init__(self, shape, eps=1e-5):
         super().__init__()
         self.eps = eps
-        self.gamma = nn.Parameter(torch.randn(shape))
-        self.beta = nn.Parameter(torch.randn(shape))
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.beta = nn.Parameter(torch.zeros(shape))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.gamma * (x - x.mean(-1, keepdim=True)) / (torch.sqrt(x.var(-1, unbiased=True ,keepdim=True)) + self.eps) + self.beta
+        return self.gamma * (x - x.mean(-1, keepdim=True)) / (torch.sqrt(x.var(-1, unbiased=False ,keepdim=True)) + self.eps) + self.beta
         
