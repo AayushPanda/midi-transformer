68347d940a75c4b00074f6f8ce3d8cc5df28ae21
Aayush Panda <aayush.vinayak@gmail.com>
2026-01-26 17:30:31 -0500

    add activations that I forgot to add to MLP

--- feed_forward_mlp.py
+++ feed_forward_mlp.py
@@ -8,9 +8,13 @@ class MLP(nn.Module):
     def __init__(self, shape: List[int], *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         depth = len(shape)
-        self.layers = nn.Sequential(*[
-            nn.Linear(shape[i], shape[j]) for i,j in zip(range(depth-1), range(1, depth+1))
-        ])
+        layers = []
+        for i in range(len(shape)-1):
+            self.layers.append(nn.Linear(shape[i], shape[i+1]))
+            if i < len(shape)-2:
+                self.layers.append(nn.GELU())
+        self.layers = nn.Sequential(*layers)
+        
 
     def forward(self, x):
         return self.layers(x)
